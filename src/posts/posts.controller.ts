ce98d33e37e8522674dd6cfcf5c90d6f17d5b571
canine89 <phk707kr@gmail.com>
2024-02-02 22:24:20 +0900

    controller와 service 분리

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,121 +1,41 @@
 import { Controller, Get, NotFoundException, Param, Post, Body, Put, Delete } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import { PostModel, PostsService } from './posts.service';
 
 // nest g resource -> 모듈 생성
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  like: number;
-  comments: string[];
-  commentsCount: number;
-}
-
-let posts = [
-  {
-    id: 1,
-    author: "아이기스",
-    title: "[공지] 전사 공지입니다",
-    content: "사실 공지 없습니다.",
-    like: 888,
-    comments: ["조아용", "좋습니다"],
-    commentsCount: 431232,
-  },
-  {
-    id: 2,
-    author: "케이트",
-    title: "[계약을 하자] 계약입니다",
-    content: "사실 계약 없습니다.",
-    like: 888,
-    comments: ["조아용", "좋습니다"],
-    commentsCount: 431232,
-  },
-  {
-    id: 3,
-    author: "바이블",
-    title: "[품의를 하자] 품의입니다",
-    content: "사실 품의 없습니다.",
-    like: 888,
-    comments: ["조아용", "좋습니다"],
-    commentsCount: 431232,
-  },
-]
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) { }
 
   @Get()
-  getPosts(): PostModel[] {
-    return posts;
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
 
   // /posts/:id
   @Get(":id")
-  getPost(@Param("id") id: string): PostModel {
-    const findedPost = posts.find((post) => post.id === +id);
-    if (!findedPost) {
-      throw new NotFoundException();
-    }
-    return findedPost;
+  getPost(@Param("id") id: string) {
+    return this.postsService.getPostById(id);
   }
 
   // /posts
   @Post()
   postPost(
-    @Body("author") author: string, @Body("title") title: string, @Body("content") content: string): PostModel {
-    const newPost = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      like: 0,
-      comments: [],
-      commentsCount: 0,
-    }
-    posts.push(newPost);
-    return newPost;
+    @Body("author") author: string, @Body("title") title: string, @Body("content") content: string) {
+    return this.postsService.createPost(author, title, content);
   }
 
   // /posts/:id
   @Put(":id")
   putPost(
     @Param("id") id: string, @Body("author") author: string, @Body("title") title: string, @Body("content") content: string
-  ): PostModel {
-    const findPost = posts.find((post) => post.id === +id);
-
-    if (!findPost) {
-      throw new NotFoundException();
-    }
-
-    if (author) {
-      findPost.author = author;
-    }
-
-    if (title) {
-      findPost.title = title;
-    }
-
-    if (content) {
-      findPost.content = content;
-    }
-
-    posts = posts.map(prevPost => prevPost.id === +id ? findPost : prevPost);
-
-    return findPost;
+  ) {
+    return this.postsService.editPost(author, title, content, id);
   }
 
   // /posts/:id
   @Delete(":id")
-  deletePost(@Param("id") id: string): PostModel {
-    const findPost = posts.find((post) => post.id === +id);
-    if(!findPost){
-      throw new NotFoundException();
-    } else{
-      posts = posts.filter(post => post.id !== +id);
-    }
-    return findPost;
+  deletePost(@Param("id") id: string) {
+    return this.postsService.deletePost(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,104 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+    id: number;
+    author: string;
+    title: string;
+    content: string;
+    like: number;
+    comments: string[];
+    commentsCount: number;
+}
+
+let posts = [
+    {
+        id: 1,
+        author: "아이기스",
+        title: "[공지] 전사 공지입니다",
+        content: "사실 공지 없습니다.",
+        like: 888,
+        comments: ["조아용", "좋습니다"],
+        commentsCount: 431232,
+    },
+    {
+        id: 2,
+        author: "케이트",
+        title: "[계약을 하자] 계약입니다",
+        content: "사실 계약 없습니다.",
+        like: 888,
+        comments: ["조아용", "좋습니다"],
+        commentsCount: 431232,
+    },
+    {
+        id: 3,
+        author: "바이블",
+        title: "[품의를 하자] 품의입니다",
+        content: "사실 품의 없습니다.",
+        like: 888,
+        comments: ["조아용", "좋습니다"],
+        commentsCount: 431232,
+    },
+]
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    getAllPosts(): PostModel[] {
+        return posts;
+    }
+
+    getPostById(id: string): PostModel {
+        const findedPost = posts.find((post) => post.id === +id);
+        if (!findedPost) {
+            throw new NotFoundException();
+        }
+        return findedPost;
+    }
+
+    createPost(author: string, title: string, content: string): PostModel {
+        const newPost = {
+            id: posts[posts.length - 1].id + 1,
+            author,
+            title,
+            content,
+            like: 0,
+            comments: [],
+            commentsCount: 0,
+        }
+        posts.push(newPost);
+        return newPost;
+    }
+
+    editPost(author: string, title: string, content: string, id: string): PostModel {
+        const findPost = posts.find((post) => post.id === +id);
+
+        if (!findPost) {
+            throw new NotFoundException();
+        }
+
+        if (author) {
+            findPost.author = author;
+        }
+
+        if (title) {
+            findPost.title = title;
+        }
+
+        if (content) {
+            findPost.content = content;
+        }
+
+        posts = posts.map(prevPost => prevPost.id === +id ? findPost : prevPost);
+
+        return findPost;
+    }
+
+    deletePost(id: string): PostModel {
+        const findPost = posts.find((post) => post.id === +id);
+        if (!findPost) {
+            throw new NotFoundException();
+        } else {
+            posts = posts.filter(post => post.id !== +id);
+        }
+        return findPost;
+    }
+}
